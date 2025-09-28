1ef6ab18689fadca54e0fb1a6bd7cae225534b6c
roman <romanbackof@gmail.com>
2025-09-28 18:12:34 +0000

    lab3

---
+++ lab3/main.py
@@ -0,0 +1,106 @@
+import random
+
+class RebbeCypher:
+    def __init__(self, bits=42):
+        self.open_key, self.close_key = self.generate_key(bits)
+
+        # гарантируем, что p != q
+        while self.close_key[0] == self.close_key[1]:
+            self.open_key, self.close_key = self.generate_key(bits)
+
+    @staticmethod
+    def extended_gcd(a, b):
+        if a == 0:
+            return 0, 1
+        else:
+            x, y = RebbeCypher.extended_gcd(b % a, a)
+            return y - (b // a) * x, x
+
+    @staticmethod
+    def mod(k, b, m):
+        i = 0
+        a = 1
+        v = []
+        while k > 0:
+            v.append(k % 2)
+            k = (k - v[i]) // 2
+            i += 1
+        for j in range(i):
+            if v[j] == 1:
+                a = (a * b) % m
+                b = (b * b) % m
+            else:
+                b = (b * b) % m
+        return a
+
+    @staticmethod
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    @classmethod
+    def generate_prime(cls, bits):
+        while True:
+            num = random.getrandbits(bits)
+            if num % 4 == 3 and cls.is_prime(num):
+                return num
+
+    @classmethod
+    def generate_key(cls, bits):
+        p = cls.generate_prime(bits)
+        q = cls.generate_prime(bits)
+        open_key = p * q
+        close_key = (p, q) # tuple for closed key
+        return open_key, close_key
+
+    @staticmethod
+    def find_Yp_Yq(p, q):
+        x, y = RebbeCypher.extended_gcd(p, q)
+        if x < 0:
+            x += q
+        Yp = x
+        Yq = (1 - Yp * p) // q
+        return Yp, Yq
+
+    def encrypt_char(self, char):
+        number = ord(char)
+        return (number ** 2) % self.open_key
+
+    def decrypt_char(self, c):
+        p, q = self.close_key
+        x, y = self.find_Yp_Yq(p, q)
+
+        while x * p + y * q != 1:
+            x, y = self.find_Yp_Yq(p, q) # решаем расширенный алгоритм Евлкида(коэффициенты находим) 1 = gcd(p,q) == 1 всегда(простые числа)
+
+        r = self.mod((p + 1) // 4, c, p)
+        s = self.mod((q + 1) // 4, c, q)
+        r1 = (x * p * s + y * q * r) % self.open_key
+        r2 = self.open_key - r1
+        r3 = (x * p * s - y * q * r) % self.open_key
+        r4 = self.open_key - r3
+
+        for item in (r1, r2, r3, r4):
+            if 0 <= item < 0x110000:  # 0x10FFFF + 1, максимальный код символа в Python
+                return chr(item)
+        return "�"
+
+
+with open("input.txt", "r", encoding='utf-8') as f:
+    text = f.read()
+
+cipher = RebbeCypher(bits=42)
+
+with open("enc.txt", "w", encoding='utf-8') as f_enc, \
+     open("output.txt", "w", encoding='utf-8') as f_dec:
+
+    for char in text:
+        encrypted_char = cipher.encrypt_char(char)
+        f_enc.write(str(encrypted_char))
+        decrypted_char = cipher.decrypt_char(encrypted_char)
+        f_dec.write(decrypted_char)
+
